c1a66d9a6c4d66f267c6178eafee328e9105240f
Eziocdl <eziocdl@gmail.com>
2024-05-05 15:02:03 -0300

    Implements package chess and UI

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,13 @@
 package application;
 
-import bordgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		ChessMatch chessMatch = new ChessMatch();
+		Ui.printBoard(chessMatch.getPieces());
 	}
 
 }
