f2a699c596c3ac8dfb0bf59291097cce6bdfef76
Eziocdl <eziocdl@gmail.com>
2024-05-12 13:46:04 -0300

    implementing possible moves of pieces and king

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,11 +24,12 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean [][] possibleMoves = chessMatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessMatch.getPieces(), possibleMoves);
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
-				
-				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
 			} catch(ChessException e) {
 				System.out.println(e.getMessage());
@@ -37,7 +38,9 @@ public class Program {
 				System.out.println(e.getMessage());
 				sc.nextLine();
 			}	
+			
+			UI.clearScreen();
 		}
-	}
+	}  
 
 }
